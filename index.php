9e809208f2eead2d56d463beb85dbcbbb0fd99d4
allianz-ITtech-solutions <parfaitalliance@gmail.com>
2024-08-27 17:56:23 +0100

    Creation d'un objet 'Professeur' (objet d'une classe fille)  et Hydration de l'objet

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 // En Java, pour utiliser une classe il faut l'importer.
 // En PHP, on parle d'Inclusion. Il faut inclure la classe
 require_once "Personne.php";
+require_once "Professeur.php"; // On inclut d'abord la classe Mère avant d'inclure Professeur qui est la fille
 
 
 $personne = new Personne();
@@ -19,7 +20,14 @@ $personne->setNomComplet("Bbw");
 // Comme en Java ou quand on affiche un objet, il fait directement appel à la méthode 'toString'
 echo $personne;
 
-// Pour exécuter un projet PHP, on peut utiliser le serveur interne de PHP.
-// on tape ceci : php -S localhost:8000 index.php
+// Instanciation d'un Objet
+$prof = new Professeur();
+
+// Hydratation
+$prof->setId(2);
+$prof->setNomComplet("Bbw2");
+$prof->setGrade("Ingénieur");
+
+echo $prof;
 
 ?>
