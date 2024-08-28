f595f08f0ba0806491e68a8083f4fd025bc49dbb
allianz-ITtech-solutions <parfaitalliance@gmail.com>
2024-08-28 01:04:16 +0100

    Met à jour 'index.php' pour appeler le Controller de 'PersonneController.php'

--- index.php
+++ index.php
@@ -4,12 +4,8 @@
 // C'est l'équivalent du 'App.java' avec la méthode main.
 // Tout sera chargé ici.
 
-
-// En Java, pour utiliser une classe il faut l'importer.
-// En PHP, on parle d'Inclusion. Il faut inclure la classe
-require_once "Personne.php";
-require_once "Professeur.php"; // On inclut d'abord la classe Mère avant d'inclure Professeur qui est la fille
-
-
+// Index fait appel au Controller
+// C'est comme quand App fait appel aux Services en Java
+require_once "./controllers/PersonneController.php";
 
 ?>
