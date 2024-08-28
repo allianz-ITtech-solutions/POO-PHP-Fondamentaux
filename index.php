916cbe0a7dad4feb441c7e0d1288cf2ae1d7197e
allianz-ITtech-solutions <parfaitalliance@gmail.com>
2024-08-28 01:09:39 +0100

    Appelle PersonneController et crée un objet Controller puis sa méthode pour lister les personnes

--- index.php
+++ index.php
@@ -8,4 +8,11 @@
 // C'est comme quand App fait appel aux Services en Java
 require_once "./controllers/PersonneController.php";
 
+// Création d'un Objet Controller
+// Il va nous fournir les services nécessaires
+$ctrl = new PersonneController();
+
+// Appel de la méthode pour lister les personnes
+$ctrl->listerPersonnes();
+
 ?>
